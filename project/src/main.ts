b277a309db8c11feba75a6f682f77b1d4e0330cd
Jakab Wolosz <jakab.wolosz@oryxgaming.com>
2022-12-14 20:02:23 +0100

    solve day 6

---
+++ project/src/main.ts
@@ -0,0 +1,90 @@
+import {input1, input2, input3, input4, input5} from "./input";
+
+export class Main
+{
+    constructor()
+    {
+        //this.firstHalf();
+        this.secondHalf();
+    }
+
+    private secondHalf()
+    {
+        const input = input5;
+        for(let i=0; i<input.length-14; i++)
+        {
+            const uniques: string[] = [];
+            const lastIndex = i+14;
+            let foundSolution = true;
+            for(let j=i; j<lastIndex; j++)
+            {
+                let alreadyIn = false;
+                for(let k=0; k<uniques.length; k++)
+                {
+                    if(input[j] === uniques[k])
+                    {
+                        alreadyIn = true;
+                        break;
+                    }
+                }
+
+                if(!alreadyIn)
+                {
+                    uniques.push(input[j]);
+                }
+                else
+                {
+                    foundSolution = false;
+                    break;
+                }
+            }
+
+            if(foundSolution)
+            {
+                console.log(i+14);
+                break;
+            }
+        }
+    }
+
+    private firstHalf()
+    {
+        const input = input5;
+        for(let i=0; i<input.length-4; i++)
+        {
+            const uniques: string[] = [];
+            const lastIndex = i+4;
+            let foundSolution = true;
+            for(let j=i; j<lastIndex; j++)
+            {
+                let alreadyIn = false;
+                for(let k=0; k<uniques.length; k++)
+                {
+                    if(input[j] === uniques[k])
+                    {
+                        alreadyIn = true;
+                        break;
+                    }
+                }
+
+                if(!alreadyIn)
+                {
+                    uniques.push(input[j]);
+                }
+                else
+                {
+                    foundSolution = false;
+                    break;
+                }
+            }
+
+            if(foundSolution)
+            {
+                console.log(i+4);
+                break;
+            }
+        }
+    }
+}
+
+new Main();
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+	entry: './project/src/main.ts',
+	mode: 'development',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, './project/build'),
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+			}
+		]
+	},
+	devtool: 'source-map'
+};
